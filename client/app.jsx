56b9f738867960b7485543aa2740c2fadc150deb
Laura <laura.robertson64@gmail.com>
2019-01-30 17:29:50 -0600

    connect submit to a future get request

--- client/app.jsx
+++ client/app.jsx
@@ -7,7 +7,9 @@ class Cart extends React.Component {
 
   render() {
     return(
-      <F1 />
+      <div>
+        <F1 />
+      </div>
     )
   }
 }
@@ -24,26 +26,38 @@ function Checkout (props) {
     )
 }
 
-function F1 (props) {
-  return (
-    <form action="/" method="POST">
-      <div>
-        <label>Name:</label>
-        <input type="text" placeholder="name"></input>
-      </div>
-      <div>
-        <label>Email:</label>
-        <input type="email" placeholder="Email"></input>
-      </div>
-      <div>
-        <label>Password:</label>
-        <input type="password" placeholder="password"></input>
-      </div>
-      <div>
-        <input type="submit"></input>
-      </div>
-    </form>
-  )
+class F1 extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  sendForm () {
+    event.preventDefault();
+    console.log('this is the form speaking')
+    axios.get()
+  }
+
+  render() {
+    return (
+      <form>
+        <div>
+          <label>Name:</label>
+          <input type="text" id="name" placeholder="name"></input>
+        </div>
+        <div>
+          <label>Email:</label>
+          <input type="email" id="email" placeholder="Email"></input>
+        </div>
+        <div>
+          <label>Password:</label>
+          <input type="password" id="password" placeholder="password"></input>
+        </div>
+        <div>
+          <button type="button" onClick={this.sendForm}>Log In</button>
+        </div>
+      </form>
+    )
+ }
 }
 
 function F2 (props) {
@@ -51,26 +65,26 @@ function F2 (props) {
     <form action="/" method="POST">
       <div>
         <label>Shipping Address:</label>
-        <input type="text" placeholder="Street address"></input>
+        <input type="text" id="address1" placeholder="Street address"></input>
       </div>
       <div>
         <label>Shipping Address Line 2:</label>
-        <input type="text" placeholder="Apt, Suit, Etc."></input>
+        <input type="text" id="address2" placeholder="Apt, Suit, Etc."></input>
       </div>
       <div>
         <label>City:</label>
-        <input type="text" placeholder="City"></input>
+        <input type="text" id="city" placeholder="City"></input>
       </div>
       <div>
         <label>State:</label>
-        <input type="text" placeholder="State"></input>
+        <input type="text" id="state" placeholder="State"></input>
       </div>
       <div>
         <label>Zipcode:</label>
-        <input type="number" placeholder="Zipcode"></input>
+        <input type="number" id="zipcode" placeholder="Zipcode"></input>
       </div>
       <div>
-        <input type="submit"></input>
+        <input type="submit" onClick={() => {alert('this is you')}}></input>
       </div>
     </form>
   )
@@ -81,28 +95,28 @@ function F3 (props) {
     <form action="/" method="POST">
       <div>
         <label>Credit Card #:</label>
-        <input type="number" placeholder="CC#" max="19"></input>
+        <input type="number" id="cc" placeholder="CC#" max="19"></input>
       </div>
       <div>
         <label>Expiry Date:</label>
-        <input type="date" min="2019-01-31" max="2030-12-31"></input>
+        <input type="date" id="expdate" min="2019-01-31" max="2030-12-31"></input>
       </div>
       <div>
         <label>CVV:</label>
-        <input type="number" placeholder="123" max="3"></input>
+        <input type="number" id="cvv" placeholder="123" max="3"></input>
       </div>
       <div>
         <label>Billing Zipcode:</label>
-        <input type="number" placeholder="Zipcode" max="5"></input>
+        <input type="number" id="billzip" placeholder="Zipcode" max="5"></input>
       </div>
       <div>
-        <input type="submit"></input>
+        <input type="submit" onClick={() => {alert('payment successful!')}}></input>
       </div>
     </form>
   )
 }
 
 ReactDOM.render(
-  <Cart />,
+  <F1 />,
   document.getElementById('hello')
 );
--- server.js
+++ server.js
@@ -1,11 +1,20 @@
+const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
 const port = 8000;
 
 app.use(express.static('./public'))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }))
+
 
 app.get('/', (req, res) => {
   res.send('index.html');
+})
+
+app.post('/', (req, res) => {
+  //console.log(req)
+  console.log('here is the body', req.body)
 })
 
 app.listen(port, () => {
